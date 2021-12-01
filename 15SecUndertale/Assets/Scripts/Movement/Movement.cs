ad4c44fc56f801278f76bba5a8398cad4dd6d7fe
Xelagro <70694180+Xelagro@users.noreply.github.com>
2021-12-01 15:10:12 +0100

    Movement Script

--- 15SecUndertale/Assets/Scripts/Movement/Movement.cs
+++ 15SecUndertale/Assets/Scripts/Movement/Movement.cs
@@ -5,8 +5,33 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
 
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    Vector2 movement;
+
+    // Update zal de inpute in komen.
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        if (movement.x != 0 || movement.y != 0)
+        {
+            animator.SetFloat("LastHorizontal", Input.GetAxisRaw("Horizontal"));
+            animator.SetFloat("LastVertical", Input.GetAxisRaw("Vertical"));
+        }
+    }
+    
+    // FixedUpdate zal de movement in komen.
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
